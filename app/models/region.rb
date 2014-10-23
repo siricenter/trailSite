8f7166d9e05612bc24d4e3cfb6593ff7af2e4ec1
ChadCarey <chadcarey7@gmail.com>
2014-10-22 21:51:06 -0600

    regions validates number ranges

--- app/models/region.rb
+++ app/models/region.rb
@@ -1,3 +1,6 @@
 class Region < ActiveRecord::Base
 	validates :name, :latitude, :longitude, :zoom, presence:true
+	validates :zoom, :numericality => { :greater_than => 0, :less_than_or_equal_to => 16 }
+	validates :latitude, :numericality => { :greater_than_or_equal_to => -90, :less_than_or_equal_to => 90 }
+	validates :longitude, :numericality => { :greater_than_or_equal_to => -180, :less_than_or_equal_to => 180 }
 end
