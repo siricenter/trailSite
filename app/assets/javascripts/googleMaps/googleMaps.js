e02d8cb253998c4b63f2a76d02fac4c8099e297c
ChadCarey <chadcarey7@gmail.com>
2014-11-02 11:21:14 -0700

    added functions to googleMaps.js to allow dynamic map changes and function adding, add marker now also returns a reference to the added marker

--- app/assets/javascripts/googleMaps/googleMaps.js
+++ app/assets/javascripts/googleMaps/googleMaps.js
@@ -1,5 +1,6 @@
     /* globals */
 	var map;
+	var mapOptions;
 	var markerArray = [];
 
 	/*constants*/
@@ -16,7 +17,7 @@
 	*/
       function initialize() {
 
-        var mapOptions = {
+        mapOptions = {
         	center: { lat: 38, lng: -100},
         	zoom: 4,
         	scrollwheel: false,
@@ -33,15 +34,13 @@
         // creates the map
         map = new google.maps.Map(document.getElementById('map-canvas'),
             mapOptions);
-
-        // add a marker to the center
-		addMarker(mapOptions.center.lat, mapOptions.center.lng, "center");
       }
       
 
 	/**
 	* addMarker will add a single marker to the map with listener functions
 	* take the latitude, longitude, and title of the marker	
+	* returns the new marker
 	*/
 	function addMarker(lat, lng, title) {
 		var myLatlng = new google.maps.LatLng(lat,lng);
@@ -52,18 +51,46 @@
 			title: title,
 			icon: PIN_IMAGE
 		});
-		
-		// add a listener to the newly created marker
-		google.maps.event.addListener(marker, 'click', function() {
-			alert(marker.title);
-			removeAllMarkers();
-		});
 
 		// add the marker to the end of the array
 		markerArray.push(marker);
 		return marker;
 	}
 
+	/**
+	* sets the map zoom to the specified value
+	*/
+	function setZoom(zoomValue) {
+		map.setZoom(zoomValue);
+	}
+
+	/**
+	* sets the map to a new center
+	*/
+	function setCenter(latitude, longitude) {
+		map.setCenter(latitude, longitude);
+	}
+
+	/**
+	* sets the center of the map and its zoom level
+	*/
+	function setMap(latitude, longitude, zoom) {
+		mapOptions.center.lat = latitude; 
+		mapOptions.center.lng = longitude;
+        mapOptions.zoom = zoom;
+        map.setOptions(mapOptions);
+	}
+
+	/**
+	* resets the center of the map and its zoom level to the default possition
+	*/
+	function resetMap() {
+		setMap(38, -100, 4);
+	}	
+
+	/**
+	* adds the specificed function to the specificed marker
+	*/
 	function addClickListenerToMarker(marker, onClickFunction) {
 		google.maps.event.addListener(marker, 'click', onClickFunction);
 	}
@@ -72,13 +99,39 @@
 	* removes all of the map markers
 	*/
 	function removeAllMarkers() {
+		alert("started removeAllMarkers()");
 		var marker = markerArray.pop();
 		while(marker != null) {
 			marker.setMap(null);
 			alert(marker.title);
 			marker = markerArray.pop();
 		}
+		alert("ended removeAllMarkers()");
+	}
+
+
+	/***test functions***/
+	function testClickFunction() {
+		alert("calling testClickFunction()");
+		removeAllMarkers();
+		populate();
+	}
+
+	function populate() {
 		for(var i = 0; i < 10; ++i) {
-			addMarker(38, -150+(i*11), "added"+(i+1));
+			newMarker = addMarker(38, -150+(i*5), "added"+(i+1));
+			addClickListenerToMarker(newMarker, testSetMap);
 		}
+	}
+
+	function testSetZoom() {
+		if(map.zoom == 4) {
+			setZoom(3);
+		} else {
+			setZoom(4);
+		}
+	}
+
+	function testSetMap() {
+		setMap(38, -50, 3);
 	}
