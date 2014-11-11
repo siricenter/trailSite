9282f4c883cefdd5aaa3a272b3ea36694017c74d
ChadCarey <chadcarey7@gmail.com>
2014-11-10 21:21:10 -0700

    added view elements to the sports_route

--- app/models/sport_route.rb
+++ app/models/sport_route.rb
@@ -1,5 +1,8 @@
 class SportRoute < ActiveRecord::Base
 	validates :name, :wall, :danger_rating, :bolts, :stars, :pitches, :yds_grade, presence:true
+	validates :zoom, :numericality => { :greater_than => 0, :less_than_or_equal_to => 16 }
+	validates :latitude, :numericality => { :greater_than_or_equal_to => -90, :less_than_or_equal_to => 90 }
+	validates :longitude, :numericality => { :greater_than_or_equal_to => -180, :less_than_or_equal_to => 180 }
 	validates :stars, :numericality => { :greater_than => 0, :less_than_or_equal_to => 5 }
 	validates :yds_grade, inclusion: { in: ["5.1", "5.2", "5.3", "5.4", "5.5", "5.6", "5.7", "5.8", "5.9", 
 		"5.10a", "5.10b", "5.10c", "5.10d", 
@@ -9,5 +12,6 @@ class SportRoute < ActiveRecord::Base
 		"5.14a", "5.14b", "5.14c", "5.14d",
 		"5.15a", "5.15b", "5.15c", "5.15d"] }
 	validates :danger_rating, inclusion: { in: ["none", "PG", "PG-13", "R", "X"] }
+	validates :anchor, inclusion: { in: ["none", "Shuts", "Rings", "Chains", "Bolts"] }
   	belongs_to :wall
 end
--- app/models/trail_type.rb
+++
@@ -1,2 +0,0 @@
-class TrailType < ActiveRecord::Base
-end
