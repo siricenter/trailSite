ff3ad5b5620462416b45bc761d94a73aa982c172
ChadCarey <chadcarey7@gmail.com>
2014-12-03 14:31:10 -0700

    now have valid sessions incoperated into the states test

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -9,17 +9,39 @@ RSpec.describe SessionsController, :type => :controller do
     end
   end
 
-  describe "GET create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
+  describe "POST create" do
+    it "as an admin redirects to admin dashboard" do
+      user = FactoryGirl.build(:user, user_type: 'admin')
+      password = user.password
+      username = user.username
+      user.save!
+      post :create, { username: username, password: password }
+      expect(response).to redirect_to '/admin'
+    end
+
+    it "as an manager redirects to admin dashboard" do
+      user = FactoryGirl.build(:user, user_type: 'manager')
+      password = user.password
+      username = user.username
+      user.save!
+      post :create, { username: username, password: password }
+      expect(response).to redirect_to '/admin'
+    end
+
+    it "as an defualt redirects to home page" do
+      user = FactoryGirl.build(:user, user_type: 'defualt')
+      password = user.password
+      username = user.username
+      user.save!
+      post :create, { username: username, password: password }
+      expect(response).to redirect_to '/home'
     end
   end
 
-  describe "GET destroy" do
-    it "returns http success" do
-      get :destroy
-      expect(response).to have_http_status(:success)
+  describe "DELETE destroy" do
+    it "returns http success and redirects to home" do
+      delete :destroy
+      expect(response).to redirect_to '/home'
     end
   end
 
