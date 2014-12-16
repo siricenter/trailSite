0c44566a5c4d987bfde0be11bf18234b95c2cc6d
ChadCarey <chadcarey7@gmail.com>
2014-12-16 10:37:10 -0700

    static.js: now if no data-marker is defined it will create a default map

--- app/assets/javascripts/static.js
+++ app/assets/javascripts/static.js
@@ -2,22 +2,27 @@
 //= require googleMaps/googleTrailMarkers
 
 function initializeMap() {
-  // get data-mapOptions attribute
   // get data-markers attribute
   var markerData = document.getElementById("map-canvas").getAttribute("data-markers");
-  markerData = JSON.parse(markerData);
-  console.dir(markerData);
+  if(markerData !== "") {
+    markerData = JSON.parse(markerData);
+    console.dir(markerData);
 
-  // make map acording to attributes (if any)
-  var map = new GoogleMapController(markerData.parent /*contains latitude, longitude, and zoom data*/);
-  
-  // add markers to the map
-  var children = markerData["children"];
-  for(var i = 0; i < children.length; ++i) {
-    var child = children[i];
-    var marker = map.addMarker(child.latitude, child.longitude, child.name);
-    // add function to each marker to call the next url (url + id)
-    map.addLink(marker, markerData["child_url"] + "/" + child.id);
+    // make map acording to attributes (if any)
+    var map = new GoogleMapController(markerData.parent /*contains latitude, longitude, and zoom data*/);
+    
+    // get data-mapOptions attribute
+    // if options exist then change attributes
+
+    // add markers to the map
+    var children = markerData["children"];
+    for(var i = 0; i < children.length; ++i) {
+      var child = children[i];
+      var marker = map.addMarker(child.latitude, child.longitude, child.name);
+      // add function to each marker to call the next url (url + id)
+      map.addLink(marker, markerData["child_url"] + "/" + child.id);
+    }
+  } else {
+    var map = new GoogleMapController();
   }
-
 }
