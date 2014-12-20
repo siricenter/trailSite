659af26718d85d913bee0b54258aff9a52dfb81c
ChadCarey <chadcarey7@gmail.com>
2014-12-20 14:06:52 -0700

    all routes now allow nil values for latitude, longitude, and zoom level

--- app/models/sport_route.rb
+++ app/models/sport_route.rb
@@ -4,10 +4,10 @@ require 'anchor_type'
 
 class SportRoute < ActiveRecord::Base
 	validates :name, :wall, :danger_rating, :bolts, :stars, :pitches, :yds_grade, presence:true
-	validates :zoom, :numericality => { :greater_than => 0, :less_than_or_equal_to => 16 }
+	validates :zoom, :numericality => { :greater_than => 0, :less_than_or_equal_to => 16 }, allow_nil: true
 	validates :bolts, numericality: { :greater_than_or_equal_to => 0 }
-	validates :latitude, :numericality => { :greater_than_or_equal_to => -90, :less_than_or_equal_to => 90 }
-	validates :longitude, :numericality => { :greater_than_or_equal_to => -180, :less_than_or_equal_to => 180 }
+	validates :latitude, :numericality => { :greater_than_or_equal_to => -90, :less_than_or_equal_to => 90 }, allow_nil: true
+	validates :longitude, :numericality => { :greater_than_or_equal_to => -180, :less_than_or_equal_to => 180 }, allow_nil: true
 	validates :stars, :numericality => { :greater_than => 0, :less_than_or_equal_to => 5 }
 	validates :yds_grade, inclusion: { in: YdsGrade.getArray() }
 	validates :danger_rating, inclusion: { in: DangerRating.getArray() }
--- app/models/traditional_route.rb
+++ app/models/traditional_route.rb
@@ -3,9 +3,9 @@ require 'danger_rating'
 
 class TraditionalRoute < ActiveRecord::Base
 	validates :name, :wall, :danger_rating, :stars, :pitches, :yds_grade, :anchor, :length, presence:true
-	validates :zoom, :numericality => { :greater_than => 0, :less_than_or_equal_to => 16 }
-	validates :latitude, :numericality => { :greater_than_or_equal_to => -90, :less_than_or_equal_to => 90 }
-	validates :longitude, :numericality => { :greater_than_or_equal_to => -180, :less_than_or_equal_to => 180 }
+	validates :zoom, :numericality => { :greater_than => 0, :less_than_or_equal_to => 16 }, allow_nil: true
+	validates :latitude, :numericality => { :greater_than_or_equal_to => -90, :less_than_or_equal_to => 90 }, allow_nil: true
+	validates :longitude, :numericality => { :greater_than_or_equal_to => -180, :less_than_or_equal_to => 180 }, allow_nil: true
 	validates :stars, :numericality => { :greater_than => 0, :less_than_or_equal_to => 5 }
 	validates :yds_grade, inclusion: { in:  YdsGrade.getArray() }
 	validates :danger_rating, inclusion: { in: DangerRating.getArray() }
