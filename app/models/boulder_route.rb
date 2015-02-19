2c48f3462c60f38bdaf0215328262bd488d5c530
evancaldwell <evancaldwell@yahoo.com>
2015-02-19 01:23:29 +0000

    forgot to resolve conflicts in models.

--- app/models/boulder_route.rb
+++ app/models/boulder_route.rb
@@ -4,13 +4,9 @@ require 'hueco_grade'
 class BoulderRoute < ActiveRecord::Base
   validates :name, :wall, :danger_rating, :pads, :length, presence:true
   validates :zoom, :numericality => { :greater_than => 0, :less_than_or_equal_to => 21 }, allow_nil: true
-<<<<<<< HEAD
-    validates :danger_rating, inclusion: { in: DangerRating.getArray() }
-=======
 #   validates :latitude, :numericality => { :greater_than_or_equal_to => -90, :less_than_or_equal_to => 90 }, allow_nil: true
 #   validates :longitude, :numericality => { :greater_than_or_equal_to => -180, :less_than_or_equal_to => 180 }, allow_nil: true
   validates :danger_rating, inclusion: { in: DangerRating.getArray() }
->>>>>>> fromV46
   belongs_to :wall
   has_many :boulder_route_photos
     belongs_to :grade
--- app/models/sport_route.rb
+++ app/models/sport_route.rb
@@ -5,11 +5,8 @@ class SportRoute < ActiveRecord::Base
 	validates :name, :wall, :danger_rating, :bolts, :pitches, :length, presence:true
 	validates :zoom, :numericality => { :greater_than => 0, :less_than_or_equal_to => 16 }, allow_nil: true
 	validates :bolts, numericality: { :greater_than_or_equal_to => 0 }
-<<<<<<< HEAD
-=======
 # 	validates :latitude, :numericality => { :greater_than_or_equal_to => -90, :less_than_or_equal_to => 90 }, allow_nil: true
 # 	validates :longitude, :numericality => { :greater_than_or_equal_to => -180, :less_than_or_equal_to => 180 }, allow_nil: true
->>>>>>> fromV46
 	validates :danger_rating, inclusion: { in: DangerRating.getArray() }
 	validates :anchor, inclusion: { in: AnchorType.getArray() }
 	belongs_to :wall
