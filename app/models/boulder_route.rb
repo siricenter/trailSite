5e1ec8f443f2de91271b314769ed4249d924d69e
evancaldwell <evancaldwell@yahoo.com>
2015-02-19 01:38:50 +0000

    unindented a couple of lines on bourlder routes model.

--- app/models/boulder_route.rb
+++ app/models/boulder_route.rb
@@ -9,6 +9,6 @@ class BoulderRoute < ActiveRecord::Base
   validates :danger_rating, inclusion: { in: DangerRating.getArray() }
   belongs_to :wall
   has_many :boulder_route_photos
-    belongs_to :grade
-    has_many :boulder_routes
+	belongs_to :grade
+	has_many :boulder_routes
 end
