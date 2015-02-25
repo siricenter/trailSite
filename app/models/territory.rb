4468cb0fa6084e20569ef4659a480c02cc7b3f26
DanielRMiller <d.miller1.work@gmail.com>
2015-02-25 17:34:22 +0000

    fixed model to allow further zooming

--- app/models/territory.rb
+++ app/models/territory.rb
@@ -1,6 +1,6 @@
 class Territory < ActiveRecord::Base
 	validates :name, :latitude, :longitude, :zoom, :area, presence:true
-	validates :zoom, :numericality => { :greater_than => 0, :less_than_or_equal_to => 16 }
+	validates :zoom, :numericality => { :greater_than => 0, :less_than_or_equal_to => 21 }
 	validates :latitude, :numericality => { :greater_than_or_equal_to => -90, :less_than_or_equal_to => 90 }
 	validates :longitude, :numericality => { :greater_than_or_equal_to => -180, :less_than_or_equal_to => 180 }
   	belongs_to :area
